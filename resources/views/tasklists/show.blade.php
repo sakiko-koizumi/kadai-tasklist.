44c1ed8bf26c5ef507944cfe0d91f4e4049011ea
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-05-29 23:31:00 +0000

    コメント

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -6,13 +6,26 @@
 
     <h1>id = {{ $tasklist->id }} のタスク詳細ページ</h1>
 　　
-　　<p>ステータス: {{ $tasklist->status }}</p>
-    <p>{{ $tasklist->content }}</p>
+　　 <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasklist->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $tasklist->status }}</td>
+        </tr>
+        <tr>
+            <th>タスクリスト</th>
+            <td>{{ $tasklist->content }}</td>
+        </tr>
+    </table>
+         {!! link_to_route('tasklists.edit', 'このタスクリストを編集', ['id' => $tasklist->id], ['class' => 'btn btn-default']) !!}
     
-     {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id]) !!}
-    
-    {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-warning']) !!}
     {!! Form::close() !!}
+    
+
      
 @endsection
