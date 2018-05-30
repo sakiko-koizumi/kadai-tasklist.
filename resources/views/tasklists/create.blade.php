00ddd6ef48fe0ebac093ab97247e41cc01c09e51
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-05-30 23:15:50 +0000

    boot

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -14,12 +14,12 @@
 
    <div class="form-group">
          {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status',null, ['class' => 'form-control'])!!} !!} !!}
    </div>   
   
    <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control'])!!}
    </div>
         {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
